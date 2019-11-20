ccdaeaab0a95d6fda98489c42fc2e16dc98d66c2
Oleksandr Bieliakov <abel9k@gmail.com>
2019-11-20 11:46:16 +0100

    UDP task from tutorial done

---
+++ src/tuto5/example/Factorial.java
@@ -0,0 +1,10 @@
+package tuto5.example;
+
+public class Factorial {
+
+    public static int compute(int n) {
+        if (n == 1 || n == 0) return 1;
+        return n * compute(n - 1);
+    }
+
+}
