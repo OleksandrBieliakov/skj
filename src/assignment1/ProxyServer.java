a0a06a0f7dc67baa5a61e1aea38c89d0867c81bb
Oleksandr Bieliakov <abel9k@gmail.com>
2019-11-30 12:09:27 +0100

    Proxy server implemented - does not work

---
+++ src/assignment1/ProxyServer.java
@@ -0,0 +1,37 @@
+package assignment1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ProxyServer {
+
+    private void listenSocket() {
+        ServerSocket proxyServerSocket = null;
+        Socket clientSocket = null;
+        try {
+            proxyServerSocket = new ServerSocket(50000);
+        } catch (IOException e) {
+            System.out.println("Could not listen");
+            System.exit(-1);
+        }
+        System.out.println("Server listens on port: " + proxyServerSocket.getLocalPort());
+
+        while (true) {
+            try {
+                clientSocket = proxyServerSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Accept failed");
+                System.exit(-1);
+            }
+            new ProxyThread(clientSocket).start();
+        }
+    }
+
+    public static void main(String[] args) {
+        ProxyServer server = new ProxyServer();
+        server.listenSocket();
+    }
+
+}
---
+++ src/assignment1/ProxyThread.java
@@ -0,0 +1,112 @@
+package assignment1;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+
+public class ProxyThread extends Thread {
+
+    private Socket clientSocket;
+    private static int nextThreadID = 0;
+    private int threadID;
+
+    ProxyThread(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    private synchronized void setID() {
+        threadID = nextThreadID++;
+    }
+
+    private void closeClientSocket() throws IOException {
+        clientSocket.close();
+        setID();
+    }
+
+    public void run() {
+        try {
+            BufferedReader clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter clientOut = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            String request = clientIn.readLine();
+            System.out.println("(" + threadID + ")First line: " + request);
+
+            String[] request_parts;
+
+            if (request == null) {
+                closeClientSocket();
+                return;
+            }
+            request_parts = request.split(" ");
+            if (!request_parts[0].equals("CONNECT")) {
+                closeClientSocket();
+                return;
+            }
+
+            String line;
+            while (!(line = clientIn.readLine()).isEmpty() || line != null) {
+                System.out.println("(" + threadID + ")Next line: " + line);
+            }
+            /*
+            for(int i=0;i<5;i++){
+                System.out.println("(" + threadID + ")Next line: " + clientIn.readLine());
+            }
+             */
+
+            String[] addressAndPort = request_parts[1].split(":");
+            System.out.println("(" + threadID + ")Connecting to host: " + Arrays.toString(addressAndPort) + "\n");
+
+            InetAddress address = InetAddress.getByName(addressAndPort[0]);
+            Socket serverSocket = new Socket(address, Integer.parseInt(addressAndPort[1]));
+
+            clientOut.println("HTTP/1.1 200 OK");
+
+            clientIn.close();
+            clientOut.close();
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        byte[] buffer = new byte[4096];
+                        int read;
+                        do {
+                            read = clientSocket.getInputStream().read(buffer);
+                            if (read > 0) {
+                                serverSocket.getOutputStream().write(buffer, 0, read);
+                                if (clientSocket.getInputStream().available() < 1) {
+                                    serverSocket.getOutputStream().flush();
+                                }
+                            }
+                        } while (read >= 0);
+                    } catch (IOException e) {
+                        System.out.println("(" + threadID + ")Error during communication");
+                    }
+                }
+            }).start();
+
+            byte[] buffer = new byte[4096];
+            int read;
+            do {
+                read = serverSocket.getInputStream().read(buffer);
+                if (read > 0) {
+                    clientSocket.getOutputStream().write(buffer, 0, read);
+                    if (serverSocket.getInputStream().available() < 1) {
+                        clientSocket.getOutputStream().flush();
+                    }
+                }
+            } while (read >= 0);
+
+            closeClientSocket();
+            serverSocket.close();
+
+        } catch (UnknownHostException e) {
+            System.out.println("(" + threadID + ")Unknown host");
+        } catch (IOException e) {
+            System.out.println("(" + threadID + ")Error during communication");
+        }
+    }
+
+}
